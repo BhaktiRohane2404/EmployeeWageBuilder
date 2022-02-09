7840bb23515535e15f03d457df15381f68270166
BhaktiRohane2404 <98884739+BhaktiRohane2404@users.noreply.github.com>
2022-02-09 06:35:34 -0800

    Add files via upload

---
+++ EmployeewagebuilderUC.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class EmployeewagebuilderUC {
+        public static void main(String[] args) {
+            System.out.println("Welcome to the employee wage computation program.");
+
+        }
+
+}
